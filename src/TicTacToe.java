3643fa3ffd11c1e6833ea4ab3dd4339a75f5e75b
nguyen5 <khanhvn296@gmail.com>
2024-03-18 10:42:24 +0100

    new method printBoard and addXO

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,9 +2,25 @@ public class TicTacToe {
 
     private char[][] gameField = new char[3][3];
 
-    public TicTacToe(){
+    public void addXO(int x, int y, char XorO){
+        gameField[x][y] = XorO;
+    }
+    public void printBoard(){
+        for(int x =0; x< gameField.length;x++){
+            for (int y = 0; y< gameField.length;y++){
+                gameField[x][y]='-';
+            }
+        }
+        for(int i = 0; i< gameField.length; i++){
+            System.out.println("| ");
+            for(int j = 0; j< gameField.length;j++){
+                System.out.println(gameField[i][j]+" | ");
+            }
+        }
+
 
     }
+
 
 
 
