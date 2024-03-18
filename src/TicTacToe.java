b9dff9dfa713fa47f626777334d9976bd8d1f2b8
krejci3 <ondra.krejci13@gmail.com>
2024-03-18 10:22:37 +0100

    created new method public void printField()

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,7 +6,26 @@ public class TicTacToe {
 
     }
 
+    public void printField(){
 
+        for(int a = 0; a < gameField.length; a++){
+            System.out.print("\t" +(a));
+        }
+
+        int i = 0;
+        System.out.print("\n");
+
+        for (int y = 0; y < gameField.length; y++) {
+            for (int x = 0; x < gameField[y].length; x++) {
+                if(x % 10 == 0){
+                    System.out.print(i);
+                }
+                System.out.print("\t" + gameField[y][x]);
+            }
+            i++;
+            System.out.println();
+        }
+    }
 
 
 }
