17a6cec4cf71bca2d36d4f6017e2c24a00293f12
krejci3 <ondra.krejci13@gmail.com>
2024-03-18 10:47:12 +0100

    created new method public String checkWinner() created new final char player1 = 'X' created new final char player2 = '0'

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class TicTacToe {
 
     private char[][] gameField = new char[3][3];
+
+    private final char player1 = 'X';
+    private final char player2 = '0';
 
     public TicTacToe(){
 
@@ -26,6 +31,62 @@ public class TicTacToe {
             System.out.println();
         }
     }
+
+    public String checkWinner(){
+        String line = null;
+
+        for (int a = 0; a < 8; a++) {
+            switch (a) {
+                case 0:
+                    line = String.valueOf(gameField[0][0] + gameField[0][1]+ gameField[0][2]);
+                    break;
+                case 1:
+                    line = String.valueOf(gameField[1][0] + gameField[1][1]+ gameField[1][2]);
+                    break;
+                case 2:
+                    line = String.valueOf(gameField[2][0] + gameField[2][1]+ gameField[2][2]);
+                    break;
+                case 3:
+                    line = String.valueOf(gameField[0][0] + gameField[1][0]+ gameField[2][0]);
+                    break;
+                case 4:
+                    line = String.valueOf(gameField[0][1] + gameField[1][1]+ gameField[2][1]);
+                    break;
+                case 5:
+                    line = String.valueOf(gameField[0][2] + gameField[1][2]+ gameField[2][2]);
+                    break;
+                case 6:
+                    line = String.valueOf(gameField[0][0] + gameField[1][1]+ gameField[2][2]);
+                    break;
+                case 7:
+                    line = String.valueOf(gameField[0][2] + gameField[1][1]+ gameField[2][0]);
+                    break;
+            }
+
+            if (line.equals("XXX")) {
+                return "X";
+            }
+
+            // For O winner
+            else if (line.equals("OOO")) {
+                return "O";
+            }
+        }
+
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(gameField).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                return "draw";
+            }
+        }
+
+
+        return null;
+    }
+
 
 
 }
